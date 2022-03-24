af04c493446b85f972f1d6cf3b7b1fa63bc4b549
Rustem <mr.rustem071193@gmail.com>
2022-03-24 22:44:08 +0600

    table

---
+++ array.js
@@ -0,0 +1,58 @@
+const tableData = [
+    { id: 1, name: 'Чехол OEM 11199 для iPhone 11 черный', price: '1 200 ₸', count: 120, instalment: true },
+    { id: 2, name: 'Чехол Samsung Smart S View Wallet Cover EF-EA562 Зеленый', price: '17 990 ₸', count: 33, instalment: false },
+    { id: 3, name: 'Samsung EF-JS906CTEGRU для Samsung Galaxy S22 Черный', price: '13 990 ₸', count: 0, instalment: false },
+    { id: 4, name: 'Veles для iPhone 13 Pro Max прозрачный', price: '790 ₸', count: 3, instalment: true },
+    { id: 5, name: 'OEM для Apple iPhone 11 прозрачный', price: '790 ₸', count: 20, instalment: true },
+    { id: 6, name: 'OEM 11199 для iPhone 11 черный', price: '1 200 ₸', count: 55, instalment: false },
+    { id: 7, name: 'Чехол Apple MM2Y3ZM MagSafe для Apple iPhone 12 Pro', price: '36 060 ₸', count: 100, instalment: false },
+]
+
+
+
+
+function drawRow(item, index) {
+    return `
+    <tr>
+        <td>${index}</td>
+        <td>
+            <div class='name_col'>${item.name}</div>
+        </td>
+        <td>${item.price}</td>
+        <td>${item.count}</td>
+        <td>${item.instalment ? '&#9989' : ''}</td>
+    </tr>
+    `
+}
+
+function drawTable(data) {
+    return `
+    <table>
+        <tbody>
+            <tr>
+                <th>№</th>
+                <th>Название</th>
+                <th>Цена</th>
+                <th>Количество</th>
+                <th>В рассрочку</th>
+            </tr>
+            ${data.map(drawRow).join('')}
+        </tbody>
+    </table>
+    `
+}
+
+function render() {
+    const tableRef = document.getElementById('table')
+    tableRef.innerHTML = drawTable(tableData)
+}
+
+render()
+
+
+
+
+
+
+
+
